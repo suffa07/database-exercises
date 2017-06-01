d46f7acf36b7b9d927d4213f07728c08ee4c6b58
DelMonroe <delenor@gmail.com>
2017-06-01 16:40:28 -0500

    Order BY exercise: step 6 completed

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -11,12 +11,12 @@ WHERE last_name LIKE 'E%E'
 ORDER BY emp_no;
 
 
-/*SELECT first_name, last_name FROM employees
-WHERE last_name LIKE 'E%%E';
 
 SELECT first_name, last_name, birth_date, hire_date FROM employees
 WHERE birth_date LIKE '%12-25'
-AND (hire_date > 19900101 AND hire_date < 19990101);
+AND (hire_date > 19900101 AND hire_date < 19990101)
+ORDER BY  hire_date ASC;
 
+/*
 SELECT first_name, last_name FROM employees
 WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%';*/
