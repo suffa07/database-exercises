88b9a5b21a0f62518f9eba96c1b63d6e483f02de
DelMonroe <delenor@gmail.com>
2017-06-01 15:31:40 -0500

    chaining where clauses, step 5 completed

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,6 +7,9 @@ AND (first_name = 'Irena' OR first_name='Vidya' OR first_name='Maya');
 SELECT first_name, last_name FROM employees
 WHERE last_name LIKE 'E%' OR  last_name LIKE '%E';
 
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+
 /*SELECT first_name, last_name, hire_date FROM employees
 WHERE hire_date > 19900101 AND hire_date < 19990101;
 
