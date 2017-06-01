b1e813b97c1dcb6b940d1615a0122404d82721d2
DelMonroe <delenor@gmail.com>
2017-06-01 16:30:15 -0500

    Order BY exercise: step 1 completed

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,11 +2,11 @@
 
 SELECT first_name, last_name, gender FROM employees
 WHERE  gender = 'm'
-AND (first_name = 'Irena' OR first_name='Vidya' OR first_name='Maya');
+AND (first_name = 'Irena' OR first_name='Vidya' OR first_name='Maya')
+ORDER BY first_name;
 
 
-
-SELECT first_name, last_name FROM employees
+/*SELECT first_name, last_name FROM employees
 WHERE last_name LIKE 'E%' OR  last_name LIKE '%E';
 
 SELECT first_name, last_name FROM employees
@@ -17,4 +17,4 @@ WHERE birth_date LIKE '%12-25'
 AND (hire_date > 19900101 AND hire_date < 19990101);
 
 SELECT first_name, last_name FROM employees
-WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%';
+WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%';*/
