089a0cc88f94387d0e15a024b07aeca4cce9bc27
DelMonroe <delenor@gmail.com>
2017-06-02 10:00:08 -0500

    GROUP BY exercise: Updated MySQL configuration to allow GROUP BY to use more variables in the SELECT statement

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -2,11 +2,14 @@ SELECT DISTINCT title FROM titles
 ORDER BY title;
 
 
-SELECT last_name FROM employees
+SELECT last_name, first_name FROM employees
 WHERE last_name LIKE 'E%%E'
 GROUP BY last_name;
 
+SELECT @@GLOBAL.sql_mode;
 
-SELECT last_name FROM employees
+SET GLOBAL SQL_MODE = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
+
+SELECT last_name, first_name FROM employees
 WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
