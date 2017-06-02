5dfc254911888d4db63f1364fbf0315ddd559cbf
DelMonroe <delenor@gmail.com>
2017-06-02 10:38:56 -0500

    LIMIT exercise: step 3 completed

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,9 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees
+WHERE birth_date LIKE '%12-25'
+      AND (hire_date > 19900101 AND hire_date < 19991231)
+  ORDER BY hire_date
+LIMIT 5;
