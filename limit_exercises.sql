ca01bc7aa83f2a3a13258413a580b3e45b9cf793
DelMonroe <delenor@gmail.com>
2017-06-02 10:45:07 -0500

    LIMIT exercise: step 4 completed

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,8 +3,8 @@ FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
-SELECT first_name, last_name, birth_date, hire_date FROM employees
+SELECT CONCAT(first_name," ", last_name), birth_date, hire_date FROM employees
 WHERE birth_date LIKE '%12-25'
       AND (hire_date > 19900101 AND hire_date < 19991231)
   ORDER BY hire_date
-LIMIT 5;
+LIMIT 5 OFFSET 50;
