d57815bda91c2d2a9e9826d4eab719eafff1d429
DelMonroe <delenor@gmail.com>
2017-06-02 09:22:32 -0500

    GROUP BY exercise: step 5, 6 completed

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,3 +5,8 @@ ORDER BY title;
 SELECT last_name FROM employees
 WHERE last_name LIKE 'E%%E'
 GROUP BY last_name;
+
+
+SELECT last_name FROM employees
+WHERE last_name LIKE'%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
